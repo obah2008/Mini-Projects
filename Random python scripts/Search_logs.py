064a3f7af701ab3bb0dcbbd03bdab8dac780b2fc
Emmanuel Obah <eobah105@gmail.com>
2025-10-06 09:26:38 +0100

    Create Search_logs.py

---
+++ Random python scripts/Search_logs.py
@@ -0,0 +1,16 @@
+import os
+
+directory = os.getcwd()
+print(directory)
+filename = input(f"Enter a file name in the {directory} directory: ")
+
+not_found = True
+keyword = input("Enter a piece of text to look for: ")
+with open(filename, "r") as file:
+   for line in file:
+       if keyword in line:
+           print(line)
+           not_found = False
+
+if not_found == True:
+    print(f"{keyword} not found in {filename}")

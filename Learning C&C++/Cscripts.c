e130206130a4a6aab895b49b9488a32fe254d3d1
Emmanuel Obah <eobah105@gmail.com>
2026-01-22 13:48:25 +0100

    Add student discount calculator in C
    Implement a student discount calculator that applies discounts based on student and senior status.

--- Learning C&C++/Cscripts.c
+++ Learning C&C++/Cscripts.c
@@ -1,1 +1,28 @@
+//Practice problem 1: A student discount calculator
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+   bool isStudent = true;
+   bool isSenior = false;
+   float newprice = 0.0;
+   float price = 10;
+   
+   if(isStudent == true){
+
+    printf("You get a 10%% discount for being a student\n");  
+    newprice = price - (price * 0.1);
+
+    if(isSenior == true){
+        printf("Since you're a student and senior you get an extra 20%% discount\nMaking your new discount 30%%\n");
+        newprice = price - (price*0.3);
+
+    }
+
+  }
+  printf("Your new price is %.2f", newprice);
+   return 0;
+}
+
+
 

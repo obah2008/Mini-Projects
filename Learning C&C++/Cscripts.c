742b4ae7dfbe5d25ace8ed65a0b15764dbd81772
Emmanuel Obah <eobah105@gmail.com>
2026-02-14 17:03:48 +0100

    Add numpad 2D array and print function

--- Learning C&C++/Cscripts.c
+++ Learning C&C++/Cscripts.c
@@ -286,4 +286,27 @@ int main(){
     operation(500);
 }
 
+#include <stdio.h>
+//2D array[][] = {{}, {}, {}}; 
+// an array where each element is an array
+
+int main(){
+ char numpad[][3] = {{'1', '2', '3'},
+                     {'4', '5', '6'},
+                     {'7', '8', '9'}, 
+                     {' ', '0', ' '}};
+
+ for(int i = 0; i<4; i++){
+     for(int j = 0; j<3; j++){
+        printf("%c ", numpad[i][j]);
+    }
+    printf("\n");
+
+ }
+}
+
+
+
+
+
 
